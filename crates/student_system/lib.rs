14047eb5b99fd78f841b9d1aa80d3a9dc8049194
Sebastian Rojas <sebastian.rojas6@unmsm.edu.pe>
2025-06-03 15:35:14 -0500

    Feat: added [domain,application,infrastructure]

--- crates/student_system/lib.rs
+++ crates/student_system/lib.rs
@@ -1,1 +1,4 @@
-
+pub mod availability;
+pub mod crud_enrollment;
+pub mod enrollment;
+pub mod suggestion;
---
+++ crates/teacher_system/lib.rs
@@ -0,0 +1,4 @@
+pub mod designate_course_schedule;
+pub mod events;
+pub mod get_courses_teacher;
+pub mod schedule_availability;
